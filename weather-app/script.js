556f84856077be47136f1b156f3eb527e68de888
Alex Soleta <alexandrasoleta@gmail.com>
2024-05-23 11:55:40 -0400

    Refactor function to improve readability

--- weather-app/script.js
+++ weather-app/script.js
@@ -88,32 +88,11 @@ async function checkWeatherMetric(city) {
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-        //update image based on the weather condition
-        if(data.weather[0].main == "Clouds") {
-            weatherIcon.src = "images/clouds.png";
-        }
-
-        else if(data.weather[0].main == "Clear") {
-            weatherIcon.src = "images/clear.png";
-        }
-
-        else if(data.weather[0].main == "Rain") {
-            weatherIcon.src = "images/rain.png";
-        }
-
-        else if(data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "images/drizzle.png";
-        }
-
-        else if(data.weather[0].main == "Mist") {
-            weatherIcon.src = "images/fog.png";
-        }
-
-        else if(data.weather[0].main == "Snow") {
-            weatherIcon.src = "images/snow.png";
-        }
-
+        //hides error message
         document.querySelector(".error").style.display = "none";
+
+        //updates the weather icon based on the weather
+        updateImages(data);
     } 
 }
 
